80e30e07df584f12906d8e5baba81f4ec499e935
jungspin <jungspinn@gmail.com>
2023-01-18 16:12:03 +0900

    Step 7

--- SwiftUIMemo/View/MainListView.swift
+++ SwiftUIMemo/View/MainListView.swift
@@ -15,7 +15,11 @@ struct MainListView: View {
     var body: some View {
         NavigationView {
             List(store.list) { memo in
-                MemoCell(memo: memo)
+                NavigationLink{
+                    DetailView(memo: memo)
+                } label: {
+                    MemoCell(memo: memo)
+                }
             }
             .listStyle(.plain)
             .navigationTitle("My Memo") // .blar call as modifier
@@ -26,7 +30,7 @@ struct MainListView: View {
                     Image(systemName: "plus")
                 }
             }
-            .sheet(isPresented: $showComposer) {
+            .sheet(isPresented: $showComposer) { // 모달
                 ComposeView()
             }
         }
